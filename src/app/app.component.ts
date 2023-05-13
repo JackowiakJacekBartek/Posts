2777a0e0bb5527234e68389c2d3a0a341f59629e
JackowiakJacekBartek-pc <Jackowiakjacekbartek@gmail.com>
2023-05-13 14:13:28 +0200

    Aplikacja posts z json serwer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from "./services/http.service";
+import {MatDialog} from "@angular/material/dialog";
+import {DialogAddPostComponent} from "./dialog-add-post/dialog-add-post.component";
+import {DialogEditPostComponent} from "./dialog-edit-post/dialog-edit-post.component";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'posts';
+export class AppComponent implements OnInit {
+
+  constructor(private httpService: HttpService, public dialog: MatDialog) {
+  }
+
+  myPosts: Array<Post> = [];
+  isOk: boolean = true;
+
+  ngOnInit(): void {
+    this.httpService.getPosts()
+      .subscribe({
+        next: (posts) => this.myPosts = posts,
+        error: (error: HttpErrorResponse) => this.isOk = error.ok
+      });
+  }
+
+  openDialogAddPost() {
+    let openDialogRef = this.dialog.open(DialogAddPostComponent);
+
+    //Odebranie z dialogu DialogAddPostComponent
+    openDialogRef.afterClosed().subscribe(res => {
+      if (res) {
+        res.id = this.myPosts[this.myPosts.length - 1].id + 1;
+        this.myPosts.push(res);
+      }
+    })
+  }
+
+  openDialogEditPost(post: Post) {
+    //Nadanie do dialogu DialogEditPostComponent
+    let editDialogRef = this.dialog.open(DialogEditPostComponent, {
+      data: {post}
+    });
+    //Odebranie z dialogu DialogEditPostComponent
+    editDialogRef.afterClosed().subscribe(res => {
+      if (res) {
+        let index = this.myPosts.findIndex(x => x.id === res.id);
+        this.myPosts[index] = res;
+      }
+    })
+  }
+
+  deletePost(id: number) {
+    this.httpService.delPost(id).subscribe();
+
+    const indexOfObject = this.myPosts.findIndex((object) => {
+      return object.id === id;
+    });
+    this.myPosts.splice(indexOfObject, 1);
+  }
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  author: string;
+  body: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {HttpService} from "./services/http.service";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {DialogAddPostComponent} from './dialog-add-post/dialog-add-post.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import { DialogEditPostComponent } from './dialog-edit-post/dialog-edit-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogAddPostComponent,
+    DialogEditPostComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatDialogModule,
+        MatInputModule,
+        ReactiveFormsModule
+    ],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
